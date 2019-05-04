f89f119690f2e9151fe5655d6c45a2d5e7999155
Prithvi Sharma <prithsharma100@gmail.com>
2019-05-04 18:49:21 +0530

    Modify directory structure

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@
 import 'react-native';
 import React from 'react';
 import { shallow } from 'react-native-testing-library';
-import App from '../App';
+import App from './App';
 
 it('renders correctly', () => {
   expect(shallow(<App />).output).toMatchSnapshot();
