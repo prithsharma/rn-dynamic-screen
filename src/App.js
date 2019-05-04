906fab09d66b0b4e737567d4d551931c0acc5a3d
Prithvi Sharma <prithsharma100@gmail.com>
2019-05-04 19:48:44 +0530

    Initial setup: Redux

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  Platform, StyleSheet, Text, View,
-} from 'react-native';
+import { Provider } from 'react-redux';
+import { configureStore } from 'redux-starter-kit';
+// import Home from './screens/Home';
+import rootReducer from './state';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n'
-    + 'Shake or press menu button for dev menu',
+const store = configureStore({
+  reducer: rootReducer,
 });
-
-let styles;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <Provider store={store} />
   );
 }
-
-styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    marginBottom: 5,
-  },
-});
