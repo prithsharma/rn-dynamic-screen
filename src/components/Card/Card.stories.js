8f3c6c45c28c7da82a0d936d4aaed963bb3ad144
Prithvi Sharma <prithsharma100@gmail.com>
2019-05-04 19:33:42 +0530

    Add props and layout to Card

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -4,4 +4,10 @@ import { storiesOf } from '@storybook/react-native';
 import Card from './index';
 
 storiesOf('Card', module)
-  .add('default', () => <Card>Primary Card</Card>);
+  .add('default', () => (
+    <Card
+      titleText="Regian Eersel's game"
+      imgUri="https://facebook.github.io/react-native/docs/assets/favicon.png"
+      descriptionText="Regian Eersel's game"
+    />
+  ));
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,35 +1,87 @@
 import React from 'react';
 import {
-  TouchableHighlight, Text, View, StyleSheet,
+  Image,
+  TouchableHighlight,
+  Text,
+  View,
+  StyleSheet,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
 
 let styles;
 
-const Card = ({ onPress }) => (
-  <TouchableHighlight onPress={onPress}>
-    <View style={styles.container}>
-      <View style={styles.title}>
-        <Text>{'Regian Eersel\'s game'}</Text>
+function Card(props) {
+  const {
+    onPress,
+    titleText,
+    imgUri,
+    descriptionText,
+    // timestamp,
+  } = props;
+  const timestampText = 'About 30 mins ago';
+
+  return (
+    <TouchableHighlight onPress={onPress}>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.titleText}>
+            {titleText}
+          </Text>
+          <Image
+            style={styles.image}
+            source={{ uri: imgUri }}
+          />
+          <View style={styles.descriptionContainer}>
+            <Text style={styles.timestamp}>
+              {timestampText}
+            </Text>
+            <Text style={styles.descriptionText} numberOfLines={2}>
+              {descriptionText}
+            </Text>
+          </View>
+        </View>
       </View>
-    </View>
-  </TouchableHighlight>
-);
+    </TouchableHighlight>
+  );
+}
 
 Card.propTypes = {
   onPress: PropTypes.func,
+  titleText: PropTypes.string.isRequired,
+  imgUri: PropTypes.string.isRequired,
+  descriptionText: PropTypes.string,
 };
 
 
 Card.defaultProps = {
   onPress: Function.prototype,
+  descriptionText: '',
 };
 
 styles = StyleSheet.create({
   container: {
     width: '100%',
     backgroundColor: 'white',
+    padding: 10,
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    marginTop: 8,
+    marginBottom: 4,
+  },
+  descriptionText: {
+    fontSize: 16,
+  },
+  timestamp: {
+    color: 'grey',
+    fontSize: 12,
+    marginBottom: 4,
   },
 });
 
