0018da07cc6b15804c62ed1039f392c39c71be05
Prithvi Sharma <prithsharma100@gmail.com>
2019-05-05 15:32:25 +0530

    Update stories for components

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -11,19 +11,23 @@ storiesOf('Card', module)
   .add('Event', () => (
     <EventCard
       imgUri="http://res.cloudinary.com/onefc/image/upload/v1554878097/events/Bangkok-2019-05-10/upcoming"
+      showCountdown={false}
+      timestampStr="2019-05-10T11:30:00+00:00"
     />
   ))
   .add('Video', () => (
     <VideoCard
       titleText="Regian Eersel's game"
-      imgUri="https://facebook.github.io/react-native/docs/assets/favicon.png"
+      imgUri="https://i.ytimg.com/vi/OeEIn72jEqg/maxresdefault.jpg"
       descriptionText="Regian Eersel's game"
+      timestampStr="2019-05-10T11:30:00+00:00"
     />
   ))
   .add('Article', () => (
     <ArticleCard
       titleText="Regian Eersel's game"
       imgUri="https://facebook.github.io/react-native/docs/assets/favicon.png"
-      descriptionText="Regian Eersel's game"
+      descriptionText="Regian Eersel was destined for a career in sports, receiving his first"
+      timestampStr="2019-05-10T11:30:00+00:00"
     />
   ));
